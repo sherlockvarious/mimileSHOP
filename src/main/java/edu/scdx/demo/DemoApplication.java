6b0e01e19e92d116ae79a48ee08ef03e5c18681c
sherlockvarious <2737793330@qq.com>
2020-07-20 17:32:59 +0800

    完成了mybatis操作

--- src/main/java/edu/scdx/demo/DemoApplication.java
+++ src/main/java/edu/scdx/demo/DemoApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication
-
-
 @MapperScan({"edu.scdx.demo.dao"})
+@SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
--- src/test/java/edu/scdx/demo/DemoApplicationTests.java
+++ src/test/java/edu/scdx/demo/DemoApplicationTests.java
@@ -1,10 +1,14 @@
 package edu.scdx.demo;
 
+import edu.scdx.demo.dao.UserMapper;
+import edu.scdx.demo.entity.User;
+import edu.scdx.demo.entity.UserExample;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @ComponentScan("edu.scdx.demo.entity")
 @SpringBootTest
@@ -23,8 +27,21 @@ class DemoApplicationTests {
     void test(){
         String userId = "1";
         String password = "111111";
+        UserExample user  = new UserExample();
+        user.createCriteria().andUserIdEqualTo(userId).andPasswordEqualTo(password);
+        List<User> hello=userMapper.selectByExample(user);
 
-        userMapper.
+        System.out.println(hello.get(0).getEmail());
+
+        User one = new User();
+        one.setPassword("1111");
+        one.setUserId("3");
+        one.setEmail("2737793332@qq.com");
+        one.setNickName("nihaoya");
+
+        userMapper.insert(one);
+
+
     }
 
 }
