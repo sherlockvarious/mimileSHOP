6163d6e616e30ce954e341442166d92a76fe16c4
sherlockvarious <2737793330@qq.com>
2020-07-20 15:25:08 +0800

    完成了mybatis操作

--- src/main/java/edu/scdx/demo/dao/ManagerRecordMapper.java
+++ src/main/java/edu/scdx/demo/dao/ManagerRecordMapper.java
@@ -1,15 +1,28 @@
 package edu.scdx.demo.dao;
 
 import edu.scdx.demo.entity.ManagerRecord;
+import edu.scdx.demo.entity.ManagerRecordExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
 public interface ManagerRecordMapper {
+    long countByExample(ManagerRecordExample example);
+
+    int deleteByExample(ManagerRecordExample example);
+
     int deleteByPrimaryKey(String recordId);
 
     int insert(ManagerRecord record);
 
     int insertSelective(ManagerRecord record);
 
+    List<ManagerRecord> selectByExample(ManagerRecordExample example);
+
     ManagerRecord selectByPrimaryKey(String recordId);
+
+    int updateByExampleSelective(@Param("record") ManagerRecord record, @Param("example") ManagerRecordExample example);
+
+    int updateByExample(@Param("record") ManagerRecord record, @Param("example") ManagerRecordExample example);
 
     int updateByPrimaryKeySelective(ManagerRecord record);
 
